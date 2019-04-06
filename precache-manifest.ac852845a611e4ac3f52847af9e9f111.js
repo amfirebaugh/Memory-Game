fbf85309ba50099ff6bd498596b31a17cb0c20d1
Phoenicis <alliefirebaugh@gmail.com>
2019-04-06 14:21:30 -0400

    Updates

---
+++ precache-manifest.ac852845a611e4ac3f52847af9e9f111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7705582ea65b58f08a7b9de1cd8f92ed",
+    "url": "/Memory-Game/static/media/music_backgnd_faded.7705582e.jpg"
+  },
+  {
+    "revision": "30833571ecdc5cd8ce4421743e72aeff",
+    "url": "/Memory-Game/static/media/doug_logo_2.30833571.jpg"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "33b35fea3afeca4f63ab",
+    "url": "/Memory-Game/static/js/main.232f2af6.chunk.js"
+  },
+  {
+    "revision": "e8d4a825fa71105fe0bf",
+    "url": "/Memory-Game/static/js/2.5ddaa8ec.chunk.js"
+  },
+  {
+    "revision": "33b35fea3afeca4f63ab",
+    "url": "/Memory-Game/static/css/main.df470713.chunk.css"
+  },
+  {
+    "revision": "70d7500394c7842b51503707a1cfdefb",
+    "url": "/Memory-Game/index.html"
+  }
+];
